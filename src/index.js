a0ed32cbd231c2813563b4500f7e66354bf9ad61
Yevhen Komarov <e.komaroff@gmail.com>
2023-04-02 06:29:37 +0300

    fibonacci

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
-import { testSolution } from './solutions/valid-parentheses.js';
+// import { testSolution } from './solutions/valid-parentheses.js';
 
-testSolution();
+// testSolution();
+
+function fibonacci(number) {
+	function fibonacciInner(number) {
+		if (number === 0) {
+			return [0];
+		} else if (number === 1) {
+			return [0, 1];
+		} else {
+			let values = fibonacciInner(number - 1);
+			values.push(values[values.length - 1] + values[values.length - 2]);
+			return values;
+		}
+	}
+
+	return fibonacciInner(number);
+}
+
+console.log(fibonacci(0));
+console.log(fibonacci(1));
+console.log(fibonacci(2));
+console.log(fibonacci(4));
+console.log(fibonacci(15));
